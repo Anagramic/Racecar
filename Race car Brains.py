b5374aa3ca1cc75eaa9339a529848267247e6676
James Andrews <james@anagramic.co.uk>
2019-11-07 09:03:56 +0000

    edit to the coasting system but the if statement still does not work

--- Race car Brains.py
+++ Race car Brains.py
@@ -19,16 +19,21 @@ def down_pressed(down):
 def coast():
     global acceleration, velocity
     acceleration = 0
+    if velocity > -0.1:
+        if velocity <0.1:
+            veloctiy = 0
     if velocity > 0:
         velocity -= 0.1
-    else:
+    elif velocity == 0:
+        pass
+    elif velocity < 0:
         velocity += 0.1
     time.sleep(0.1)
 
 def accelerate():
-    global velocity, acceleration, accelerating
+    global velocity, acceleration, accelerating, zero_to_sixty
     accelerating = True
-    acceleration = 0.2
+    acceleration = zero_to_sixty
     velocity += acceleration
     time.sleep(0.1)
     print("Accelerating")
@@ -46,10 +51,11 @@ def brake():
     print("Braking")
 
 def main():
-    global velocity, acceleration, accelerating, braking    
+    global velocity, acceleration, accelerating, braking, zero_to_sixty    
     acceleration = 0
     accelerating = False
-    braking = False  
+    braking = False
+    zero_to_sixty = 0.2
     Height = 795
     Width = 1500
     x = 0
@@ -86,7 +92,7 @@ def main():
         point = canvas.create_oval(x,y, x + pointsize, y + pointsize, fill = '#000000' )
 
         canvas.update()
-        canvas.after(2)
+        canvas.after(1)
         canvas.delete(point)
         
 
