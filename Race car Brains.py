a51a2d832891b171f290686b852f8e890c7a7ec6
James Andrews <james@anagramic.co.uk>
2019-11-08 18:09:57 +0000

    Added a line that shows direction and speed

--- Race car Brains.py
+++ Race car Brains.py
@@ -60,7 +60,7 @@ def brake():
 def main():
     global velocity, acceleration, accelerating, braking, zero_to_sixty, turning_left, turning_right
     direction = 0
-    turning_radius = 1
+    turning_radius = 5
     turning_left = False
     turning_right = False
     acceleration = 0
@@ -69,8 +69,14 @@ def main():
     zero_to_sixty = 0.2
     Height = 795
     Width = 1500
+    grid_xsf = Width/100
+    grid_ysf = Height/100
+#staring location
     x = 0
     y = Height/2
+
+    dx = 0
+    dy = 0
     velocity = 0
     tk = Tk()
     pointsize = 10
@@ -79,11 +85,19 @@ def main():
     canvas.pack()
 
     while True:
-#        print(velocity)
-        dx = velocity*math.acos(math.radians(direction))
-        dy = velocity*math.asin(math.radians(direction))
+#        print('--------------------------------------------------------------------------------')
+#        print('v: '+str(velocity))
+#        print('x: '+str(x))
+#        print('y: '+str(y))
+#        print('dx: '+str(dx))
+#        print('dy: '+str(dy))
+#        print('Direction: '+str(direction))
+        dx = velocity*math.cos(math.radians(direction))
+        dy = velocity*math.sin(math.radians(direction))
         x = x + dx
         y = y + dy
+#        print(x)
+#        print(y)
 
         if accelerating == True:
             accelerate()
@@ -97,12 +111,14 @@ def main():
 
         if turning_left == True:
             direction -= turning_radius
-
+            turning_left = False
         if turning_right == True:
             direction += turning_radius
-        
+            turning_right = False       
+
         if x >= Width or y >= Height:
-             x = -x
+             x = 0
+             y = 0
              acceleration = 0
              
         tk.bind("<Left>", turn_left)
@@ -110,11 +126,12 @@ def main():
         tk.bind("<Up>", up_pressed)
         tk.bind("<Down>", down_pressed)
         
+        arrow = canvas.create_line(x,y,x+(dx*5),y+(dy*5),fill = '#000000')
         point = canvas.create_oval(x,y, x + pointsize, y + pointsize, fill = '#000000' )
 
         canvas.update()
         canvas.after(1)
-        canvas.delete(point)
+        canvas.delete(point,arrow)
         
 
 main()
